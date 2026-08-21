d09fe9156454a26040fbbead4ef1b146a0b50aee
50gnr <77696975+50gnr@users.noreply.github.com>
2026-08-21 10:17:22 -0600

    Update userSetup.js

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -1,10 +1,17 @@
 function userMarkdownSetup(md) {
+
   // The md parameter stands for the markdown-it instance used throughout the site generator.
   // Feel free to add any plugin you want here instead of /.eleventy.js
+
 }
+
 function userEleventySetup(eleventyConfig) {
-  // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
-  // Feel free to add any plugin you want here instead of /.eleventy.js
+
+  eleventyConfig.addPassthroughCopy({
+    "src/site/audio": "audio"
+  });
+
 }
+
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
